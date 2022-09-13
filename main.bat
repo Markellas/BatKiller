2c175298bf0beff11812795f6cac4c6b87e0ef64
Markella's <105635627+Markellas@users.noreply.github.com>
2022-09-13 18:24:07 +0300

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,11 +1,17 @@
 @echo off
 
 echo Welcome to BatKiller made by Markella's!
-
+echo.
 echo ----------------------------------------
-
+echo.
 echo Please, select the way you want to torment your Windows.
-
+echo.
 echo Ways to torment Windows:\n1. Create 1M folders
+echo.
 
-set /P way=Enter the number: 
+set /P way=Enter the number:
+
+if %way%==1 echo Creating 1M folders. 
+if %way%==1 echo Creating 1M folders..
+if %way%==1 echo Creating 1M folders...
+if %way%==1 echo Пон
